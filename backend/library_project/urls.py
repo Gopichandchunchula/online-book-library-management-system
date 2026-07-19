bad7bdc38ad0a49d758293d0bb81f2397b6e6c8f
chunchula Gopichand <gopichandchunchula@gmail.com>
2026-07-19 11:09:58 +0530

    change the urls file

--- backend/library_project/urls.py
+++ backend/library_project/urls.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 from . import auth_views
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="National Harold Campus Library (LibraManage) API",
-      default_version='v1',
-      description="Unified back-end server endpoint mapping for active circulation, asset holds, fine ledgers, and system configurations.",
-      terms_of_service="https://www.libramanage.com/terms/",
-      contact=openapi.Contact(email="support@libramanage.com"),
-      license=openapi.License(name="Enterprise BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="National Harold Campus Library (LibraManage) API",
+#       default_version='v1',
+#       description="Unified back-end server endpoint mapping for active circulation, asset holds, fine ledgers, and system configurations.",
+#       terms_of_service="https://www.libramanage.com/terms/",
+#       contact=openapi.Contact(email="support@libramanage.com"),
+#       license=openapi.License(name="Enterprise BSD License"),
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+# )
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -35,9 +35,9 @@ urlpatterns = [
     path('api/borrowings/', include('borrowings.urls')),
     
     # Swagger API Interactive documentation views
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
